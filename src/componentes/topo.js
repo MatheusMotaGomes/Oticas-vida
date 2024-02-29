bcecc892ca4ba43563a6911c595d19c717633a88
Matheus Mota <matheusmgf2010@gmail.com>
2024-02-29 11:53:12 -0300

    responsivo 550px
    atualizei o responsivo até 550px

--- src/componentes/topo.js
+++ src/componentes/topo.js
@@ -9,7 +9,7 @@ export default function Topo(){
           <img src={Logo} alt="logo"/>
         </div>
 
-        <div>
+        <div id='div-2'>
           <a href='#produtos'>PRODUTOS</a>
           <a href='#sobre'>SOBRE</a>
           <a href='#contato'>CONTATO</a>
